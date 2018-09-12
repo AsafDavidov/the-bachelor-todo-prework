662d3bea0dc0909867efeddf01ab335627d9f77c
Asaf Davidov <adavidov@conncoll.edu>
2018-09-12 16:37:58 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -5,6 +5,7 @@ def get_first_name_of_season_winner(data, season)
   data["#{season}"].each do |contestant, data|
     if contestant["status"] == "Winner"
       winner = contestant["name"]
+    end
   end
   winner
 end
