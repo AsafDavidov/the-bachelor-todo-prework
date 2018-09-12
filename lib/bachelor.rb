2c03ac1e32fe0dc0c3e411c0002e49c820d01c2f
Asaf Davidov <adavidov@conncoll.edu>
2018-09-12 16:30:26 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,5 +1,10 @@
 def get_first_name_of_season_winner(data, season)
   # code here
+  winner = ""
+  if data["#{season}"]["status"] == "Winner"
+  end
+  winner = data["#{season}"]["status"]
+  winner
 end
 
 def get_contestant_name(data, occupation)
