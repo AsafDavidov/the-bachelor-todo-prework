b8f18d3c837a607b801c64e41ee620667ab1f02c
Asaf Davidov <adavidov@conncoll.edu>
2018-09-12 16:41:35 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -12,7 +12,8 @@ end
 
 def get_contestant_name(data, occupation)
   # code here
-  data.each do |season, contestant
+  data.each do |season, contestant|
+    contestant.select do contestant["occupation"] == occupation
 end
 
 def count_contestants_by_hometown(data, hometown)
