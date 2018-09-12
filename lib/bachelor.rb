a5c54a08e723e778ce6a57684457d1ea13ad11e0
Asaf Davidov <adavidov@conncoll.edu>
2018-09-12 17:03:58 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -55,7 +55,7 @@ def get_average_age_for_season(data, season)
   data["#{season}"].each_with_index do |contestant, index|
     all_ages_array[index] = contestant["age"].to_f
   end
-  sum = 0
+  sum = 0.0
   all_ages_array.each do |number|
     sum += number
   end
